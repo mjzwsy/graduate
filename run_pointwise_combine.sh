ed1a8608ef8defbabf021886283f64a22cf63629
ubuntu <ubuntu@localhost.localdomain>
2023-04-16 14:51:12 +0800

    run sh file

---
+++ run_pointwise_combine.sh
@@ -0,0 +1,2 @@
+python allrank/combine_main.py --config-file-name allrank/config_pointwise_combine.json --run-id run_test --job-dir run_test
+#/home/ubuntu/.conda/envs/allrank/lib/python3.7/site-packages/allrank/models/losses/listMLE.py
